17eb232a1e93b2ded1e97b7440e56aba827fd402
cnekals24 <120033122+cnekals24@users.noreply.github.com>
2024-09-26 21:45:16 +0900

    week 3

---
+++ week3/실습 3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) 
+{
+    char c;
+
+    printf("enter a character: ");
+    scanf("%c", &c);
+    
+   
+    printf("the next character of %c (%d) is %c (%d)\n", c, c, c + 1, c + 1);
+
+    return 0;
+}
+
+
