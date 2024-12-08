98f773d81176e99a1631db9c7055f5a0eeeba1c5
cnekals24 <120033122+cnekals24@users.noreply.github.com>
2024-12-08 23:41:45 +0900

    sh

--- week12/12-3.c
+++ week12/12-3.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
 
-void swap(int *x, int *y) {
-    int temp = *x;
-    *x = *y;
-    *y = temp;
-}
+int main(void){
+    int i;
+    int grade[5];
 
-int main(void) {
-    int a = 3;
-	int b = 5;
-    swap(&a, &b);
-    printf("a:%i, b:%i\n", a, b);
+    grade[0] = 10;
+    grade[1] = 20;
+    grade[2] = 40;
+    grade[3] = 30;
+    grade[4] = 60;
+    
+    for(i=0;i<5,i++)   
+        printf("grade[%d] = %d\n",i,grade[i]);
     return 0;
-}
+} 
